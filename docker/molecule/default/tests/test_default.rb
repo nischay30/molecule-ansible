f6506b6ecbac9bfd64673f58140e34825eb547f7
Nischay Goyal <nischaygoyal@macusers-MacBook-Pro.local>
2019-01-01 22:37:00 +0000

    initial test case for docker

---
+++ docker/molecule/default/tests/test_default.rb
@@ -0,0 +1,12 @@
+# Molecule managed
+
+# describe file('/etc/hosts') do
+#   its('owner') { should eq 'root' }
+#   its('group') { should eq 'root' }
+#   its('mode') { should cmp '0644' }
+# end
+
+describe docker.version do
+  its('Server.Version') { should cmp >= '1.12' }
+  its('Client.Version') { should cmp >= '1.12' }
+end
